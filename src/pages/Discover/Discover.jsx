9abaf6a3175817fa85dea551bebabd5d29c21dad
noven latigo <77792034+noven21@users.noreply.github.com>
2022-10-10 18:46:42 +0800

    fetching data from api and redux toolkit file

--- src/pages/Discover/Discover.jsx
+++ src/pages/Discover/Discover.jsx
@@ -1,13 +1,28 @@
 import React from 'react';
 import { HeaderDiscover } from '../../components/Header/Header';
 import Navbar from '../../components/Navbar/Navbar';
+import { useDispatch, useSelector } from 'react-redux';
+
+import { useGetTopChartsQuery } from '../../redux/services/shazamCore';
 
 const Discover = () => {
+	const dispatch = useDispatch();
+	const { data, isFetching, error } = useGetTopChartsQuery();
+	const genreTitle = 'Pop';
+
+	if (isFetching) return <p>Loading...</p>;
+
+	if (error) return <p>Error...</p>;
+
+	console.log(data);
+
 	return (
-		<div>
+		<section>
 			<Navbar />
 			<HeaderDiscover />
-		</div>
+
+			<div></div>
+		</section>
 	);
 };
 
