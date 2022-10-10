586e30a101a0a3f154083563e9a319cc60a6343d
noven latigo <77792034+noven21@users.noreply.github.com>
2022-10-10 16:00:17 +0800

    creating navbar component and adding styles

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,15 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { links } from '../../assets/constants';
 import { NavLink } from 'react-router-dom';
+import { RiCloseLine } from 'react-icons/ri';
+import { HiOutlineMenu } from 'react-icons/hi';
 
 const NavLinks = () => (
-	<div>
+	<div className='flex flex-col md:flex-row gap-10'>
 		{links.map((item) => (
-			<NavLink
-				className='flex flex-row justify-between'
-				key={item.name}
-				to={item.to}
-			>
+			<NavLink className='font-bold' key={item.name} to={item.to}>
 				{item.icon}
 				{item.name}
 			</NavLink>
@@ -18,13 +16,42 @@ const NavLinks = () => (
 );
 
 const Navbar = () => {
+	const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
+
 	return (
 		<nav>
-			<div className='flex-col justify-between'>
+			<div className='hidden md:flex flex-col item-center justify-start md:justify-between md:flex-row'>
 				<div>
-					<h3>vibenMusic</h3>
+					<h3 className='text-lg font-bold text-cyan-500'>
+						vibenMusic
+					</h3>
 				</div>
 				<NavLinks />
+			</div>
+
+			{/* Mobile */}
+			<div className='absolute md:hidden block top-6 right-3'>
+				{mobileMenuOpen ? (
+					<RiCloseLine
+						className='cursor-pointer w-6 h-6 mr-2'
+						onClick={() => setMobileMenuOpen(false)}
+					/>
+				) : (
+					<HiOutlineMenu
+						className='cursor-pointer w-6 h-6  mr-2'
+						onClick={() => setMobileMenuOpen(true)}
+					/>
+				)}
+			</div>
+			<div
+				className={`absolute top-0 h-screen w-2/3 bg-black text-white backdrop-blur-lg z-10 p-6 md:hidden smooth-transition ${
+					mobileMenuOpen ? 'left-0' : '-left-full'
+				}`}
+			>
+				<h3 className='mb-10 text-lg text-white font-bold'>
+					vibenMusic
+				</h3>
+				<NavLinks handleClick={() => setMobileMenuOpen(false)} />
 			</div>
 		</nav>
 	);
