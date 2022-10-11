6f8963967b223ff6d8803e931d048d4fe70803ef
noven latigo <77792034+noven21@users.noreply.github.com>
2022-10-11 13:07:47 +0800

    creating components for music player

---
+++ src/components/MusicPlayer/index.jsx
@@ -0,0 +1,13 @@
+import React, { useState, useEffect } from 'react';
+
+import Controls from './Controls';
+import Player from './Player';
+import Seekbar from './Seekbar';
+import Track from './Track';
+import VolumeBar from './VolumeBar';
+
+const index = () => {
+	return <div>index</div>;
+};
+
+export default index;
