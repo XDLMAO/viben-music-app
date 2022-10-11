2df9ea0b4d0c7dedb41cebf01614e37330f2c6aa
noven latigo <77792034+noven21@users.noreply.github.com>
2022-10-11 10:58:55 +0800

    fetching song data from api, displaying data to discover and passing as props to song card component, creating songCard and playpause component to render song details

---
+++ src/components/SongCard/SongCard.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import PlayPause from '../PlayPause/PlayPause';
+
+const SongCard = ({ song, index }) => {
+	const activeSong = 'test';
+
+	const handlePauseClick = () => {};
+
+	const handlePlayClick = () => {};
+
+	return (
+		<div className='flex flex-col w-[250px] p-4 bg-white/5 bg-opacity-80 backdrop-blur-sm animate-slideup rounded-lg cursor-pointer'>
+			<div className='relative w-full h-56 group '>
+				<div
+					className={`absolute inset-0 justify-center 
+    items-center bg-black bg-opacity-50 group-hover:flex ${
+			activeSong?.title === song.title
+				? 'flex bg-black bg-opacity-70'
+				: 'hidden'
+		} `}
+				>
+					<PlayPause
+						song={song}
+						handlePause={handlePauseClick}
+						handlePlay={handlePlayClick}
+					/>
+				</div>
+				<img src={song.images?.coverart} alt='song_img' />
+			</div>
+		</div>
+	);
+};
+
+export default SongCard;
--- src/pages/Discover/Discover.jsx
+++ src/pages/Discover/Discover.jsx
@@ -6,6 +6,7 @@ import { useDispatch, useSelector } from 'react-redux';
 import { useGetTopChartsQuery } from '../../redux/services/shazamCore';
 import Error from '../../components/Error/Error';
 import Loader from '../../components/Loader/Loader';
+import SongCard from '../../components/SongCard/SongCard';
 
 const Discover = () => {
 	const dispatch = useDispatch();
@@ -23,7 +24,26 @@ const Discover = () => {
 			<Navbar />
 			<HeaderDiscover />
 
-			<div></div>
+			<div className='flex flex-col'>
+				<div>
+					{/* <div>
+						<h2>Discover {genreTitle}</h2>
+						<select>
+							{genres.map((genre) => (
+								<option
+									key={genre.value}
+									value={genre.value}
+								></option>
+							))}
+						</select>
+					</div> */}
+					<div className='flex flex-wrap sm:justify-start justify-center gap-8'>
+						{data?.map((song, index) => (
+							<SongCard key={song.key} song={song} index={index} />
+						))}
+					</div>
+				</div>
+			</div>
 		</section>
 	);
 };
